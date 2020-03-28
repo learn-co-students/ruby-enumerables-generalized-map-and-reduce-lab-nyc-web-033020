9edd12db796f45b0a8df135cb75680ccf053b7cc
Garen Sahagian <g.s.sahagian@gmail.com>
2020-03-28 20:00:21 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,23 @@
 # Your Code Here
+def map(source_array)
+  index = 0 
+  new_array = []
+  while index < source_array.length do
+    new_array[index] = yield(source_array[index])
+    index += 1
+  end
+  return new_array
+end
+
+def reduce(source_array, starting_point = 0)
+  index = 0 
+  result = starting_point
+    while index < source_array.length do 
+      result = yield(result, source_array[index])
+      index += 1
+      end
+  if result == 0 
+    return true
+  end
+  result
+end
